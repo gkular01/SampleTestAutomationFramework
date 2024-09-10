a80a27f13512960255141d4def71dcd9c0f367e8
Gurpreet Kular <gurpreet.kular1@gmail.com>
2024-09-10 18:18:22 -0500

    Merge remote-tracking branch 'origin/Feature1' into Gurpreet

---
+++ src/test/java/com/api/test/CountApiTest.java
@@ -0,0 +1,8 @@
+package com.api.test;
+
+public class CountApiTest {
+	public static void main(String[]args)
+	{
+ System.out.println("countapiu");
+}
+}
--- src/test/java/com/api/test/LoginApiTest.java
+++ src/test/java/com/api/test/LoginApiTest.java
@@ -2,4 +2,7 @@ package com.api.test;
 
 public class LoginApiTest {
 
+	public void loginApiTest() {
+		System.out.println("Running Login API");
+	}
 }
